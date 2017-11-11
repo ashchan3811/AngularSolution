a32a32c1fee9bdd0bba56f082ccaeb47546b1f41
Ashwani Kumar <kumar.ashwani@live.in>
2017-11-11 10:48:12 +0530

    Created Project

--- Angular/ClientApp/app/models/item.ts
+++ Angular/ClientApp/app/models/item.ts
@@ -1,3 +1,11 @@
-﻿export interface Item {
+﻿export class Item {
+    id: string;
+    label: string;
+    type: string;
 
+    constructor(id: string, label: string, type: string) {
+        this.id = id;
+        this.label = label;
+        this.type = type;
+    }
 }
