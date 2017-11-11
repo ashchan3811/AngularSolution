d0f760c2f5492fd37060acf08be0511d574b69ee
Ashwani Kumar <kumar.ashwani@live.in>
2017-11-11 10:44:44 +0530

    Add project files.

---
+++ Angular/ClientApp/app/components/fetchdata/fetchdata.component.ts
@@ -0,0 +1,23 @@
+import { Component, Inject } from '@angular/core';
+import { Http } from '@angular/http';
+
+@Component({
+    selector: 'fetchdata',
+    templateUrl: './fetchdata.component.html'
+})
+export class FetchDataComponent {
+    public forecasts: IWeatherForecast[];
+
+    constructor(http: Http, @Inject('BASE_URL') baseUrl: string) {
+        http.get(baseUrl + 'api/SampleData/WeatherForecasts').subscribe(result => {
+            this.forecasts = <IWeatherForecast[]>result.json();
+        }, error => console.error(error));
+    }
+}
+
+export interface IWeatherForecast {
+    dateFormatted: string;
+    temperatureC: number;
+    temperatureF: number;
+    summary: string;
+}
---
+++ Angular/ClientApp/app/providers/data.service.ts
@@ -0,0 +1,14 @@
+﻿import { Injectable } from '@angular/core';
+
+
+@Injectable()
+export class DataService {
+
+    constructor() {
+
+    }
+
+    getData() {
+
+    }
+}
